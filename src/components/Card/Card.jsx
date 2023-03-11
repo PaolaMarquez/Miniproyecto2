6a40c624551c8731c0329aea80f9ba726d88a615
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-11 16:31:39 -0400

    All movies

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export default function Card({ movie }){
+    const imgUrl = "https://image.tmdb.org/t/p/original"
+    return(
+        <div className="group relative">
+            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img
+                src={`${imgUrl}${movie.poster_path}`}
+                alt={movie.title}
+                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+            </div>
+            <div className="mt-4 flex justify-between">
+            <div>
+                <h3 className="text-sm text-gray-700">
+                <Link to={movie.href}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {movie.title}
+                </Link>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">{movie.popularity}</p>
+            </div>
+            <p className="text-sm font-medium text-gray-900">{movie.original_language}</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
+import { fetchMovies } from "../../utils/movies-api";
 
 export function HomePage() {
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const {data} = await fetchMovies();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div>
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Películas</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {movies.map((movie) => (
+            <Card movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
