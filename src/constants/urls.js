14fbd3bf174529f35b64959db2e34484a9cc2bb9
samergh1 <gh.samer@correo.unimet.edu.ve>
2023-03-11 18:36:27 -0400

    Url fixed

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,4 +2,3 @@ export const HomePageUrl = "/";
 export const LoginPageUrl = "/login";
 export const RegisterPageUrl = "/register";
 export const MovieDetailPageUrl = "/detail"
-export const HomePageUrl = "/"
