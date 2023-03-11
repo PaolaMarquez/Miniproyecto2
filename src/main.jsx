04c4674575c7bdbc01a297d36a01b05443302332
samergh1 <gh.samer@correo.unimet.edu.ve>
2023-03-11 12:02:48 -0400

    Login design

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { LoginPage } from './pages/LoginPage/LoginPage';
+import { LoginPageUrl } from './constants/urls';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={LoginPageUrl} element={<LoginPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
