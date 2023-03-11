3264954a818ff82c1b93cd1c69b29b2c9c1cc26c
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-11 13:16:14 -0400

    Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import { HomePageUrl, MovieDetailPageUrl } from "./constants/urls";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { MovieDetailPage } from "./pages/MovieDetailPage/MovieDetailPage";
+import { Layout } from "./pages/Layout/Layout";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path={HomePageUrl} element={<HomePage />} />
-        <Route path={MovieDetailPageUrl} element={<MovieDetailPage />} />
+        <Route element={<Layout />}>
+          <Route path={HomePageUrl} element={<HomePage />} />
+          <Route path={MovieDetailPageUrl} element={<MovieDetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export function HomePage() {
   return (
     <div>
-      <h1 className="bg-green-400">HomePage</h1>
     </div>
   );
 }
