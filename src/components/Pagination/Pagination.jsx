dd83500d4b079ff19fa487c3850d95fe9e8243e5
samergh1 <gh.samer@correo.unimet.edu.ve>
2023-03-11 23:44:05 -0400

    Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,34 +1,7 @@
-import React, {useState} from 'react'
-import { fetchMovies } from '../../utils/movies-api';
+import React, { useState, useEffect } from 'react'
 
-export default function Pagination() {
-    const [page, setPage] = useState(1);
-    
-    const handlePrev = () => {
-        setPage(page - 1);
-        fetchMovies(page);
-    };
-
-    const handleNext = () => {
-        setPage(page + 1);
-        fetchMovies(page);
-    };
-
+export function Pagination() {
     return (
-        <div className="flex gap-4 justify-center items-center mt-10">
-            <button
-            onClick={handlePrev}
-            className="bg-[#00786A]  hover:scale-105 transition-all rounded-md text-white py-2 px-4 disabled:bg-gray-400 "
-            disabled={page === 1}
-            >
-                Prev
-            </button>
-            <button
-                onClick={handleNext}
-                className="bg-[#00786A] hover:scale-105 transition-all rounded-md text-white py-2 px-4 "
-            >
-                Next
-            </button>
-        </div>
-    )
-}
+        <div></div>
+    );
+};
