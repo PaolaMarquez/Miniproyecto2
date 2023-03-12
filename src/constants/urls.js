405c38b8b2701eb1dc83abb85a8dd011617df8fc
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-12 17:35:05 -0400

    Preview con detail

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,4 +2,4 @@ export const HomePageUrl = "/";
 export const LoginPageUrl = "/login";
 export const RegisterPageUrl = "/register";
 export const SearchPageUrl = "/search";
-export const MovieDetailPageUrl = (movieId = ":movieId") => `/movies/${movieId}`;
+export const MovieDetailPageUrl = (movieId = ":movieId") => `/movies/${movieId}`
